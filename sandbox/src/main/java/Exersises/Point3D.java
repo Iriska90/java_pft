b7bac5c83e3d52d50399ba21d1edb75474964ec5
Iryna Harmash <irina.harmash@gmail.com>
2019-03-26 11:47:51 +0200

    Создан первый тест для Addressbook

---
+++ sandbox/src/main/java/Exersises/Point3D.java
@@ -0,0 +1,12 @@
+/*package Exersises;
+
+class Point3D extends Point
+{
+  int z;
+  Point3D(int x, int y, int z)
+  {
+    super(x, y);     // Здесь мы вызываем конструктор суперкласса
+    this.z=z;
+  }
+  }
+*/
