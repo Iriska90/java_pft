c4696a1c4a22798099de2202877dddc8ed830816
Iryna Harmash <irina.harmash@gmail.com>
2019-03-26 12:05:37 +0200

    Шаги для входа на сайт из блока "Test" перенесены в блок "BeforeClass"

---
+++ sandbox/src/main/java/Exersises/Distance.java
@@ -0,0 +1,39 @@
+/*package Exersises;
+
+public class Distance {
+
+  public static void main(String args[]) {
+    Point p1 = new Point(10, 20);
+    Point p2 = new Point(42, 99);
+    System.out.println("x = " + p1.x + " у = " + p1.y);
+  }
+}
+*/
+
+/*
+package ru.stqa.pft.sandbox;
+
+public class MyFirstProgram {
+  public static void main(String[] args) {
+    hello("world");
+    hello("user");
+    hello("user2");
+
+		/*	Point p1 = new Point();
+		Point p2 = new Point();
+		p1.х = 10;
+		p1.у = 20;
+		p2.х = 42;
+		p2.у = 99;
+		System.out.println("x = " + p1.х + " у = " + p1.у);
+		System.out.println("x = " + p2.х + " у = " + p2.у);
+
+	Point p1 = new Point(3, 2);
+		p1.x = 3;
+		p1.y = 2;
+
+		Point p2 = new Point(5, 4);
+		p2.x = 5;
+		p2.y = 4;
+			System.out.println("Расстояние между двумя точками = " + distance(Point p1, Point p2);
+			*/
---
+++ sandbox/src/main/java/Exersises/PointCreate.java
@@ -0,0 +1,11 @@
+/*package Exersises;
+
+class PointCreate
+{
+  public static void main(String args[])
+  {
+    Point p = new Point(10,20);
+    System.out.println("x = " + p.x + " у = " + p.y);
+  }
+}
+*/
