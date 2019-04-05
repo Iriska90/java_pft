949fedbbbea1376373daeb45a96571e9c70e8f96
Iryna Harmash <irina.harmash@gmail.com>
2019-04-05 10:18:58 +0300

    Сделан тест для модификации групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/appmanager/GroupHelper.java
@@ -35,6 +35,14 @@ public class GroupHelper extends HelperBase {
   public void selectGroup() {
     click(By.name("selected[]"));
   }
+
+  public void initGroupModification() {
+    click(By.name("edit"));
+  }
+
+  public void submitGroupModification() {
+    click(By.name("update"));
+  }
 }
 /*
 import org.openqa.selenium.By;
